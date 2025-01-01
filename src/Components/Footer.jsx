385d0f1ac1ab1b68b0dc724381e45bd5eba4b26b
sadman-zakaria <sadicon5152@gmail.com>
2025-01-01 18:54:26 +0600

    Foooter.jsx working

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <div>
+        <h4 className='text-[#0FF1F6]'>GET STARTED</h4>
+        <h2 className='text-white text-4xl '><span className='italic'>Embrace</span> the new era of <span className='italic'>outbound</span>.</h2>
+        <p className='text-gray-500'>Wizia lets you train, activate, and optimize aiDRs.  <br />
+        Take your outbound to new levels of performance and efficiency.</p>
+
+        <button className=''>
+        Sign Up for the Beta
+            <img src="" alt="" />
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Footer
