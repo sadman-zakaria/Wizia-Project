ffe05a98c506ca3974a46943a985a5bf5685ca34
sadman-zakaria <sadicon5152@gmail.com>
2025-01-01 18:42:19 +0600

    Allocate section done

--- src/Components/Allocate.jsx
+++ src/Components/Allocate.jsx
@@ -7,9 +7,22 @@ function Allocate() {
             <div className=''>
                <h2 className=' text-3xl md:text-4xl'>Allocate effort where your reps <br /> make an inpact.</h2>
                <h4 className='text-2xl md:text-3xl italic text-[#0FF1F6]'>Let us handle the rest.</h4>
-               <p className=' text-gray-500'>Keep your reps “in the air” -- out in the field and on the phone <br /> where they can build relationships.</p>
+               <p className=' text-gray-500 pt-2'>Keep your reps “in the air” -- out in the field and on the phone <br /> where they can build relationships.</p>
 
-                
+               <div className='flex items-center pt-2 md:pt-5 gap-5'>
+                    <div>
+                       <h3 className='text-[#0FF1F6] text-2xl  md:text-3xl font-bold '>32%</h3>
+                       <p className='text-[#E9EEF1]'>Improvement in <br /> Open Rates</p>
+                    </div>
+                    <div>
+                       <h3 className='text-[#0FF1F6] text-2xl  md:text-3xl font-bold '>75%</h3>
+                       <p className='text-[#E9EEF1]'>Improvement in <br /> Ramp Time</p>
+                    </div>
+                    <div>
+                       <h3 className='text-[#0FF1F6] text-2xl  md:text-3xl font-bold '>35%</h3>
+                       <p className='text-[#E9EEF1]'>Improvement in <br /> Meetings Booked</p>
+                    </div>
+                </div>
             </div>
 
         </div>
